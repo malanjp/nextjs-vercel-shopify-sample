3d898d6857d9d15e5372d99e286f5583f13c84c9
malan <shibata.michihiro@gmail.com>
2020-10-17 22:48:42 +0900

    delete trash code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,6 @@ import { useRouter } from 'next/router'
 import jaTranslations from '@shopify/polaris/locales/ja.json';
 
 function MyApp({ Component, pageProps }) {
-  const { query } = useRouter();
-  console.log(query);
-  console.log(process.env);
-
   return (
     <React.StrictMode>
       <AppProvider i18n={jaTranslations}>
