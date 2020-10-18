fa717460dbba94d545d818969e4918e5a89d5207
malan <shibata.michihiro@gmail.com>
2020-10-18 16:35:07 +0900

    use APP_URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ Home.getInitialProps = async ({ query, res }) => {
   console.log(process.env.SHOPIFY_API_KEY);
   const { SHOPIFY_API_KEY } = process.env;
   const scopes = 'read_orders';
-  const redirect_uri = `${process.env.APP_URL}terms` || 'http://localhost:3000/terms';
+  const redirect_uri = `${process.env.APP_URL}terms`;
   const url = `https://${query.shop}/admin/oauth/authorize?client_id=${SHOPIFY_API_KEY}&scope=${scopes}&redirect_uri=${redirect_uri}`;
   console.log(url);
   if (res) {
